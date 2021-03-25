566029325bc4ee3d915d77329fc6f8fdcc778c64
freeivanzw <freeivanzw@gmail.com>
2021-03-26 01:57:11 +0200

    footer fixed bug

--- app/js/srcript.js
+++ app/js/srcript.js
@@ -3,17 +3,21 @@ $(function(){
         $(".menu").addClass("menu-active"),
         $(".menu__active-shadow").addClass("menu__active-shadow-show")
 
-    })
+    });
 
     $(".header-main__close").click(function(){
         $(".menu").removeClass("menu-active"),
         $(".menu__active-shadow").removeClass("menu__active-shadow-show")
-    })
+    });
 
     $(".menu__active-shadow").click(function(){
         $(".menu").removeClass("menu-active"),
         $(".menu__active-shadow").removeClass("menu__active-shadow-show")
-    })
+    });
+
+    $(".calculation__details").click(function(){
+        $(".calculation__show").toggleClass("calculation__show-active")
+    });
 
     var elem = $('.header-main');
     var doc = $(document);
