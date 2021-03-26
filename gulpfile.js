fab1715a5873dbbafbef321db82395119fd7498a
freeivanzw <freeivanzw@gmail.com>
2021-03-26 19:00:26 +0200

    buind project

--- gulpfile.js
+++ gulpfile.js
@@ -27,13 +27,20 @@ function imgMin () {
     .pipe(gulp.dest("./app/img/"));
 };
 
-// function build () {
-//     return gulp.src([
-//         "./app/css."
-//     ])
-// }
+function build () {
+    return gulp.src([
+        "./app/css/style.min.css",
+        "./app/css/OverlayScrollbars.css",
+        "./app/fonts/**/*",
+        "./app/img/**/*",
+        "./app/js/**/*.js",
+        "./app/index.html"
+    ],{base:"app"})
+    .pipe(gulp.dest("dist"))
+}
 
 exports.compiller = compiller;  // компиляція scss - css
 exports.watch = gulp.series(compiller, watch);  // scss вотчер-компиляція
 exports.cssmin = cssmin;        
 exports.imgmin = imgMin;
+exports.build = gulp.series(compiller, cssmin, imgMin, build);
